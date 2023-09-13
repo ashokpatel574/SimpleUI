c129f96ac569dff07a47a320bda89ec999027325
Ashok Patel <96565022+ashokpatel574@users.noreply.github.com>
2023-09-13 17:41:33 +0530

    added social link in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,39 @@
+import { Link } from "react-router-dom";
+
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
 import "./footer.css";
+
+const footerSocialIconsList = [
+  {
+    id: "1",
+    link: "https://github.com/ashokpatel574/SimpleUI",
+    Icon: GitHubIcon,
+  },
+  {
+    id: "2",
+    link: "https://www.linkedin.com/in/ashokpatel574/",
+    Icon: LinkedInIcon,
+  },
+  { id: "3", link: "https://twitter.com/ashokpatel574", Icon: TwitterIcon },
+];
 
 const Footer = () => {
   return (
     <footer className="footer_container">
+      <ul className="social_links flex-center">
+        {footerSocialIconsList.map((listItem) => {
+          const { id, link, Icon } = listItem;
+          return (
+            <li className="link_list-item" key={id}>
+              <Link className="link" to={link} target="_blank">
+                <Icon />
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
       <p className="footer_trademark">© 2023 Ashok Patel</p>
     </footer>
   );
